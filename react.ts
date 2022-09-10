936d9b9e3c5259c94d34a6f79ea2bf9e2dfc6f20
Jay Meistrich <jmeistrich@gmail.com>
2022-09-10 14:44:13 +0800

    Fix React TS errors by having ObservablePrimitive extend ReactFragment (only in React export)

--- react.ts
+++ react.ts
@@ -1,3 +1,11 @@
 export * from './src/react/observer';
 export * from './src/react/useComputed';
 export * from './src/react/useObservable';
+import type { ObservableBaseFns } from '@legendapp/state';
+import type { ReactFragment } from 'react';
+
+declare module '@legendapp/state' {
+    export interface ObservablePrimitive<T = any> extends ObservableBaseFns<T>, ReactFragment {
+        set?(value: T | ((prev: T) => T)): ObservablePrimitive<T>;
+    }
+}
